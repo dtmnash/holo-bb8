5f856ea624e2a7e0536f2f90649c73a5102e8a08
Dan Nash <dtmnash@gmail.com>
2016-09-29 01:11:02 -0500

    adding assets

---
+++ HoloBB8/Assets/Scripts/HelloBB8/PlayAudioOnInput.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAudioOnInput : MonoBehaviour {
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+    public InputType input;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnGazeEnter()
+    {
+        if(input == InputType.Gaze)
+        {
+            PlayAudioClip.PlayAudio(audioSource, audioClip);
+        }
+    }
+}
+
+public enum InputType
+{
+    Gaze,
+    HandPressed, 
+}
