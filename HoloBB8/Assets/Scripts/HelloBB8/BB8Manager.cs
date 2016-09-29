7d85ea459448be3e455dd2c97c9473452ec8686b
Dan Nash <dtmnash@hotmail.com>
2016-09-29 04:30:05 -0500

    updated BB8 scene, rotation needs some work still, and we need a reset button

---
+++ HoloBB8/Assets/Scripts/HelloBB8/BB8Manager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using HoloToolkit.Unity;
+
+public class BB8Manager : Singleton<BB8Manager>
+{
+    public AudioSource source;
+    public AudioClip walkClip;
+    public AudioClip talkClip;
+    bool dropped = false;
+    Vector3 _OriginalPosition;
+    Quaternion _OriginalRotation;
+
+    public void DoAction(string action)
+    {
+        string animation = "";
+        AudioClip audioClip = null;
+
+        switch (action)
+        {
+            default:
+                break;
+            case "Walk":
+                {
+                    animation = "BB8Walk";
+                    audioClip = walkClip;
+                    break;
+                }
+            case "Talk":
+                {
+                    animation = "BB8Talk";
+                    audioClip = talkClip;
+                    break;
+                }
+        }
+
+        var animator = gameObject.GetComponent<Animator>();
+
+        animator.Play(animation);
+        source.clip = audioClip;
+        source.Play();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        _OriginalPosition = transform.localPosition;
+        _OriginalRotation = transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnSelect()
+    {
+        if (dropped)
+        {
+            DoAction("Walk");
+        }
+        else
+        {
+            var rigidBody = gameObject.GetComponent<Rigidbody>();
+            rigidBody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+            rigidBody.useGravity = true;
+            dropped = true;
+        }
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        var rigidBody = gameObject.GetComponent<Rigidbody>();
+        rigidBody.collisionDetectionMode = CollisionDetectionMode.Discrete;
+        rigidBody.useGravity = true;
+        gameObject.transform.localRotation.Set(_OriginalRotation.x, _OriginalRotation.y, _OriginalRotation.z, _OriginalRotation.w);
+    }
+}
--- HoloBB8/Assets/Scripts/HelloBB8/PlayAudioOnInput.cs
+++ HoloBB8/Assets/Scripts/HelloBB8/PlayAudioOnInput.cs
@@ -1,31 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayAudioOnInput : MonoBehaviour {
+public class PlayAudioOnInput : MonoBehaviour
+{
     public AudioSource audioSource;
     public AudioClip audioClip;
     public InputType input;
+    bool animated = false;
     // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    
-	}
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnGazeEnter()
     {
-        if(input == InputType.Gaze)
+        if (input == InputType.Gaze)
         {
             PlayAudioClip.PlayAudio(audioSource, audioClip);
+
+            if (!animated)
+            {
+                var animator = gameObject.GetComponent<Animator>();
+                if (!animator)
+                    throw new System.Exception("AAH");
+
+                animator.Play("BB8Talk");
+            }
         }
+    }
+
+    void OnSelect()
+    {
+
     }
 }
 
 public enum InputType
 {
     Gaze,
-    HandPressed, 
+    HandPressed,
 }
